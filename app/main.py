0b3d11ae0006fc21e84e45d8a20dacf707677f79
Ezequiel Parziale <eparziale@gmail.com>
2022-12-06 19:33:15 -0300

    :rocket: update: minor changes
    -add task schema
    -add delete task endpoint

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ from fastapi.responses import JSONResponse
 
 from .config import settings
 
-from .worker import celery_app, create_task
+from .worker import celery_app, create_new_task
+
+from .schemas import Task
 
 # FastAPI
 app = FastAPI(
@@ -11,9 +13,9 @@ app = FastAPI(
 )
 
 
-@app.get("/")
-async def root(task_type: int):
-    task = create_task.delay(task_type)
+@app.post("/tasks")
+async def create_task(task: Task):
+    task = create_new_task.apply_async(args=[task.task_type], eta=task.started_at)
     return JSONResponse({"task_id": task.id})
 
 
@@ -26,3 +28,9 @@ def get_status(task_id):
         "task_result": task_result.result,
     }
     return JSONResponse(result)
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id):
+    task_result = celery_app.AsyncResult(task_id).revoke(terminate=True)
+    return JSONResponse(task_result)
